61804ddf8b7fc879e129ec93dd32219d9dd6fa8c
Sri Ram <sriramreddydwarampudi>
2026-05-03 16:15:01 +0000

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,23 +8,23 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-namespace = "com.ram.handyindia"
-compileSdk = flutter.compileSdkVersion
-ndkVersion = flutter.ndkVersion
+android {
+    namespace = "com.ram.handyindia"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
 
-compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 
-kotlinOptions {
-    jvmTarget = JavaVersion.VERSION_17.toString()
-}
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
 
-defaultConfig {
-    applicationId = "com.ram.handyindia"
+    defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.ram.handy"
+        applicationId = "com.ram.handyindia"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
